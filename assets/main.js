904f3d3f5aa3d159868ee247bf2a99c874376ccc
AdamBenDev <adamaya9732@gmail.com>
2023-03-13 02:02:38 +0100

    Program js done

--- assets/main.js
+++ assets/main.js
@@ -7,10 +7,9 @@ console.log("JS OK");
 3 - Creo un'istruzione condizionale.
 4 - Stampo le variabili in pagina.*/
 
-
 //!PRENDO L'ELEMENTO IN PAGINA
 
-let element = document.getElementById('element');
+let element = document.getElementById("element");
 
 //CREO UN CICLO CON I NUMERI DA 1 A 100
 
@@ -22,9 +21,23 @@ let buzz = "buzz";
 
 let content = "";
 
-for (let i=i; i<= 100; i++){
+for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log(fizzBuzz);
-        content += `
+      console.log (fizzBuzz);
+      content += `<div class="bg-custom-red fw-bold">${fizzBuzz}</div>`;
+    } else if (i % 3 === 0) {
+      console.log (fizz);
+      content += `<div class="bg-custom-green fw-bold">${fizz}</div>`;
+    } else if (i % 5 === 0) {
+      console.log (buzz);
+      content += `<div class="bg-custom-yellow fw-bold">${buzz}</div>`;
+    } else {
+      console.log(i);
+      content += `<div class="bg-custom-light-blue fw-bold">${i}</div>`;
     }
 }
+
+//! 4 - Stampo la variabile d'appoggio in pagina
+
+
+element.innerHTML = content;
